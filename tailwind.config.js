447f04caf631d0d15de6d9d4e5d58d9df42c8f98
4shktz <evilancasteras@edenschool.fr>
2023-12-20 16:06:15 +0100

    added a lot of things

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,36 +4,38 @@ export default {
   theme: {
     fontWeight: {
       "extra-light": 200,
-      light: 300,
-      normal: 400,
-      medium: 500,
-      semibold: 600,
-      bold: 700,
+      "light": 300,
+      "normal": 400,
+      "medium": 500,
+      "semibold": 600,
+      "bold": 700,
     },
     extend: {
       fontFamily: {
         spartan: ["Spartan", "sans-serif"],
       },
       colors: {
-        beaver: "#9e7f66",
-        "cod-gray": "#111",
-        mirage: "#17192b",
+        "beaver": "#9e7f66",
+        "cod-gray": "#111111",
+        "mirage": "#17192b",
         "ebony-clay": "#242b37",
         "shuttle-gray": "5c6779",
       },
       backgroundImage: {
         "hero-mobile":
           "url('images/homepage/hero-bg-mobile.jpg')",
-        "ready-mobile":
-          "url('images/homepage/ready-bg-mobile.jpg')",
         "hero-tablet":
           "url('images/homepage/hero-bg-tablet.jpg')",
-        "ready-tablet":
-          "url('images/homepage/ready-bg-tablet.jpg')",
         "hero-desktop":
           "url('images/homepage/hero-bg-desktop.jpg')",
+
+        "ready-mobile":
+          "url('images/homepage/ready-bg-mobile.jpg')",
+        "ready-tablet":
+          "url('images/homepage/ready-bg-tablet.jpg')",
         "ready-desktop":
           "url('images/homepage/ready-bg-desktop.jpg')",
+
         "hero-mobile-booking":
           "url('images/booking/hero-bg-mobile.jpg')",
         "hero-tablet-booking":
